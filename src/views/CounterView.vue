ec9f189a070a5c498d6d4246d0c6968cf259aab0
Fernanda <fe.perezl@duocuc.cl>
2024-04-09 21:39:59 -0400

    increase and decrease

---
+++ src/views/CounterView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const counter = ref(0);
+const increase = () => counter.value++;
+const decrease = () => counter.value--;
+
+
+</script>
+
+<template>
+    <h1>Counter View: {{ counter }}</h1>
+    <button @click="increase">
+        + 1
+    </button>
+    <button @click="decrease">
+        - 1
+    </button>
+</template>
