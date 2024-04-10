c3be2f794a1ba126b982cc34c93f2c2f12c6b33b
fp-roots <fe.perezl@duocuc.cl>
2024-04-10 12:48:41 -0400

    Fix console.log statement in PokemonList.vue

--- src/pokemons/pages/PokemonList.vue
+++ src/pokemons/pages/PokemonList.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+
 import pokemonApi from '../api/pokemonApi';
 
 
 pokemonApi.get('/pokemon?limit=45')
-    .then( data => console)
+    .then( data => console.log(resp.data)
 </script>
 <template>
     <div>
