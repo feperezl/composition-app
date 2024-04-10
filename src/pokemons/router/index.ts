c23f531b308b035fe26bcc18ed14e367ed9b8b5f
fp-roots <fe.perezl@duocuc.cl>
2024-04-10 11:02:03 -0400

    Add PokemonLayout component to PokemonLayout.vue Update Pokemon route in router/index.ts

--- src/pokemons/router/index.ts
+++ src/pokemons/router/index.ts
@@ -1,17 +1,24 @@
-import { compile } from 'vue';
-import type { RouteRecordRaw } from 'vue-router';
-
+import type { RouteRecordRaw } from "vue-router";
 
 const ROUTE_NAME = 'pokemons';
 
+// rutas hijas 
 export const pokemonRoute: RouteRecordRaw = {
-    path: `/${ROUTE_NAME}`,
-    component: () => import('@/pokemons/layouts/PokemonsLayout.vue'),
+    path: `/${ ROUTE_NAME }`,
+    redirect: `/${ ROUTE_NAME }/list`,
+    component: () => import('@/pokemons/layouts/PokemonLayout.vue'),
     children: [
         {
-            path: `/${ ROUTE_NAME }/list`,
-            component:() => import ('@/pokemon/pages/PokemonList.vue')
+            path: `/${ ROUTE_NAME}/by/:id`,
+            component: () => import('@/pokemons/pages/PokemonById.vue')
+        },
+        {
+            path: `/${ ROUTE_NAME}/list`,
+            component: () => import('@/pokemons/pages/PokemonList.vue')
+        },
+        {
+            path: `/${ ROUTE_NAME}/search`,
+            component: () => import('@/pokemons/pages/PokemonSearch.vue')
         }
     ]
-
 }
